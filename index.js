ed8078f0085e183282404917e30a067a4c7523d6
Martin Bing <bing.martin.photography@gmail.com>
2015-01-05 15:54:43 +0100

    Add route param with name

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.put('/', function(req, res){ res.send('put request'); });
 app.patch('/', function(req, res){ res.send('patch request'); });
 app.delete('/', function(req, res){ res.send('delete request'); });
 
+app.get('/:name', function(req, res){
+  res.send("hello, " + req.params.name)
+});
 
 app.listen(port, function(){
   console.log('listening on port ' + port);
