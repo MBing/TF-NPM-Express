9f88de126eb75179084c10c86310f0dc11d4ae0f
Martin Bing <bing.martin.photography@gmail.com>
2015-01-06 08:19:14 +0100

    Adding middleware calls and log file

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var express = require('express'),
+var connect = require('connect'),
+	basicAuth = require('basicAuth'),
+	express = require('express'),
     port = 9000;
 
 var app = express();
+
+app.use(function(req, res, next){
+  console.log('hello from the middleware!');
+  next();
+});
+
+app.use(function(req, res, next){
+  console.log('doing async stuff...');
+  setTimeout(function(){
+    console.log('done!');
+    next();
+  }, 1000);
+});
 
 app.get('/', function(req, res){ res.send('get request'); });
 app.post('/', function(req, res){ res.send('post request'); });
